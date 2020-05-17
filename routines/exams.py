e65e7c13551d91b9b2ea6d5652607c9e1986e694
Developer From Jokela <developerfromjokela@gmail.com>
2020-05-17 12:20:05 +0300

    Partially implemented exam's routines

--- routines/exams.py
+++ routines/exams.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2020 wilmaplus-notifier, developed by Developer From Jokela, for Wilma Plus mobile app
 
-from .abstract import AbstractRoutine
+from .abstract import AbstractRoutine, convertToJSON, convertFromJSON
 from wilma_connector.wilma_client import WilmaClient
 
 
@@ -13,7 +13,20 @@ class Exams(AbstractRoutine):
         exams = wilma_client.getExams()
         if not exams.is_error():
             return exams.get_exception()
+        offline_data_pt = self.get_file(enc_pass, push_id)
+        if offline_data_pt is not None:
+            offline_data = convertFromJSON(offline_data_pt)
+            for l_exam in exams:
+                found = False
+                for o_exam in offline_data:
+                    if o_exam['ExamId'] == l_exam['ExamId']:
+                        found = True
+                        break
+                if not found:
+                    # TODO send push
+                    ...
 
+        self.save_file(convertToJSON(exams), enc_pass, push_id)
         return None
 
 
