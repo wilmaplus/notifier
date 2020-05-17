a91dea5003ed519d1b009de974cea8333d15c8d0
Developer From Jokela <developerfromjokela@gmail.com>
2020-05-17 12:24:04 +0300

    Added entry for FCM HTTP Client baseURL in notifier.example.py

--- wilma_connector/http_client.py
+++ wilma_connector/http_client.py
@@ -51,3 +51,27 @@ class WilmaHttpClient:
             return RequestResult(False, None, r)
         except Exception as e:
             return ErrorResult(e)
+
+
+class FCMHttpClient:
+
+    def __init__(self):
+        self.baseUrl = wilma_url
+        self.sessionHttp = requests.Session()
+
+    def getBaseURLDomainName(self):
+        return '{uri.netloc}'.format(uri=urlparse(self.baseUrl))
+
+    def get_request(self, url):
+        try:
+            r = self.sessionHttp.get(self.baseUrl + url)
+            return RequestResult(False, None, r)
+        except Exception as e:
+            return ErrorResult(e)
+
+    def post_request(self, url, data, headers=None, followRedirects=True):
+        try:
+            r = self.sessionHttp.post(self.baseUrl + url, data=data, allow_redirects=followRedirects)
+            return RequestResult(False, None, r)
+        except Exception as e:
+            return ErrorResult(e)
