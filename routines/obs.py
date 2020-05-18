3d7a9defc20c038e6fd493340884303f31e5b538
Developer From Jokela <developerfromjokela@gmail.com>
2020-05-18 20:59:33 +0300

    Added routine for observations

--- routines/obs.py
+++ routines/obs.py
@@ -5,27 +5,27 @@ from wilma_connector.wilma_client import WilmaClient
 from wilma_connector.fcm_client import FCMClient
 
 
-class Exams(AbstractRoutine):
+class Observations(AbstractRoutine):
     def __init__(self):
-        super().__init__("exams")
+        super().__init__("observations")
 
     def check(self, wilmaserver, wilmasession, push_id, user_id):
         wilma_client = WilmaClient(wilmaserver, wilmasession)
         fcm_client = FCMClient()
-        exams = wilma_client.getExams()
-        if exams.is_error():
-            return exams.get_exception()
+        obs = wilma_client.getObservations()
+        if obs.is_error():
+            return obs.get_exception()
         offline_data_pt = self.get_file(push_id, push_id, user_id)
         if offline_data_pt is not None:
             offline_data = convertFromJSON(offline_data_pt)
-            for l_exam in exams.get_exams():
+            for l_obs in obs.get_observations():
                 found = False
-                for o_exam in offline_data:
-                    if o_exam['ExamId'] == l_exam['ExamId']:
+                for o_obs in offline_data:
+                    if o_obs['Id'] == l_obs['Id']:
                         found = True
                         break
                 if not found:
-                    push_content = {'type': 'notification', 'data': self.name, 'payload': l_exam}
+                    push_content = {'type': 'notification', 'data': self.name, 'payload': l_obs}
                     fcm_client.sendPush(push_id, push_content)
-        self.save_file(convertToJSON(exams.get_exams()), push_id, push_id, user_id)
+        self.save_file(convertToJSON(obs.get_observations()), push_id, push_id, user_id)
         return None
