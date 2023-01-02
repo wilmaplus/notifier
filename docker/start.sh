cf66e754e9a92c90e3c4c3ce09bdb50e057ad4be
Developer From Jokela <developerfromjokela@gmail.com>
2023-01-02 11:30:42 +0200

    Added fcmdaemon to start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -6,4 +6,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 /usr/sbin/crond -f -l 8 &
+python manage.py fcmdaemon &
 gunicorn --worker-class=gevent --worker-connections=$WORKER_CONNECTIONS --timeout $WORKER_TIMEOUT --access-logfile - --workers $WORKERS --bind 0.0.0.0:80 wplusnotifier.wsgi:application
